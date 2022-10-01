a17260681e392ab38af6bcc29dbb340d13077cfb
Priyanshu Kumar <32311478+priyanshu-kr@users.noreply.github.com>
2022-10-01 11:53:59 +0530

    Matrix

---
+++ matrix_lower_triangular.c
@@ -0,0 +1,34 @@
+// [print lower triangular matrix]
+/*
+#include<stdio.h>
+
+int main()
+{
+    int a[5][5],m,n,i,j;
+    printf("Enter number of rows and columns of matrix:\n");
+    scanf(" %d %d",&m,&n);
+
+    printf("Enter elements of matrix:\n");
+    for (i = 0; i<m; ++i)
+    {
+        for (j=0; j<n; ++j)
+            scanf(" %d",&a[i][j]);
+    }
+
+    printf("Lower Triangular matrix:\n");
+
+    for (i=0; i<m; ++i)
+    {
+        printf("\n");
+        for (j = 0; j < n; ++j)
+        {
+            if (i >= j)
+                printf("%d ",a[i][j]);
+            else
+                printf("0 ");
+        }
+    }
+
+    return 0;
+}
+*/
